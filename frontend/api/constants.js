9226d87c55d6dc659b05c959fccc2407330ac08a
lowkangn <kangngee62@gmail.com>
2023-06-27 11:24:56 +0800

    Rename secure-qr to frontend

---
+++ frontend/api/constants.js
@@ -0,0 +1,4 @@
+// Locally hosted ML API exposed using ngrok.
+export const BASE_URL = "https://37ef-175-156-81-175.ngrok-free.app/";
+
+export const ML_MODEL_ENDPOINT = "url_classifier";
---
+++ frontend/components/ScanResultModal.js
@@ -0,0 +1,71 @@
+import { Button, Modal, StyleSheet, Text, View } from "react-native";
+
+const ScanResultModal = ({
+  isVisible,
+  isMalicious,
+  url,
+  handleClose,
+}) => {
+  return (
+    <Modal animationType="slide" visible={isVisible} transparent={true}>
+      <View style={styles.centeredView}>
+        <View style={styles.modalView}>
+          <Text style={styles.modalText}>
+            {isMalicious ? "WARNING" : "SAFE"}
+          </Text>
+          {isMalicious ? (
+            <Text style={styles.modalText}>
+              Secure QR has detected that this QR code is malicious. Please do
+              not access the contents of this QR code.
+            </Text>
+          ) : (
+            <>
+              <Text style={styles.modalText}>
+                Secure QR did not find anything malicious about this QR code.
+              </Text>
+              <Text style={styles.modalText}>{url}</Text>
+            </>
+          )}
+          <Button
+            onPress={handleClose}
+            title={isMalicious ? "CLOSE" : "OPEN LINK"}
+          />
+          {!isMalicious && <Button onPress={handleClose} title="CANCEL"></Button>}
+        </View>
+      </View>
+    </Modal>
+  );
+}
+
+const styles = StyleSheet.create({
+  centeredView: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  modalView: {
+    margin: 8,
+    backgroundColor: "white",
+    borderRadius: 8,
+    padding: 16,
+    alignItems: "center",
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5,
+  },
+  textStyle: {
+    color: "white",
+    fontWeight: "bold",
+    textAlign: "center",
+  },
+  modalText: {
+    textAlign: "center",
+  },
+});
+
+export default ScanResultModal;
