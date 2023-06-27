8a927ebb512bbaac9904685ff345c20000bd6451
lowkangn <kangngee62@gmail.com>
2023-06-27 16:03:43 +0800

    Add ability to visit URLs

--- frontend/src/api/constants.js
+++ frontend/src/api/constants.js
@@ -1,4 +1,4 @@
 // Locally hosted ML API exposed using ngrok.
-export const BASE_URL = "https://4657-175-156-81-175.ngrok-free.app/";
+export const BASE_URL = "https://67a9-175-156-81-175.ngrok-free.app/";
 
 export const ML_MODEL_ENDPOINT = "url_classifier";
